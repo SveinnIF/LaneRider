f59d51680f4433fd7ea4d87cf0e97bd4b0f1e888
SveinnIF <SveinnIF@github.com>
2020-10-13 12:58:40 +0000

    fuck

--- LaneFollower/laneFollower.py
+++ LaneFollower/laneFollower.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 import picamera
+from easygopigo3 import EasyGoPiGo3
+import time
+
+gpg = EasyGoPiGo3()
 
 def canny(image):
     #this turns the image grey
@@ -18,27 +22,56 @@ def display_lines(image, lines):
         for line in lines:
             x1, y1, x2, y2 = line.reshape(4)
             cv2.line(line_image,(x1, y1), (x2, y2), (255,0,0),10)
+            print((x1, y1),(x2, y2))
     return line_image
 
 def waypoint_detection(image):
     NUM_IGNORED_ROWS = 60
-    NUM_WAYPOINTS = 3
+    NUM_WAYPOINTS = 1
 
     num_rows = len(image)
     num_relevant_rows = (num_rows - NUM_IGNORED_ROWS)
 
 
     step = num_relevant_rows // (NUM_WAYPOINTS + 1)
-    start = NUM_IGNORED_ROWS + step
+    start = len(image[0])-(NUM_IGNORED_ROWS + step)
     waypoints = []
-    for i in range(start, num_rows, step):
-        print(i)
+    for i in range(start, 0, -step):
         for j in range(len(image[i]) - 1, -1, -1):
             if image[i][j] == 255:
-                waypoints.append((i, j))
+                waypoints.append((i,j))
                 break
+    if(waypoints == []):
+        waypoints = [(0,len(image[0]))]
     return waypoints
 
+
+def moveGoPiGo(waypoint):
+    if ((waypoint[0][1] < (len(image[0])/2)+10) and (waypoint[0][1] > (len(image[0])/2)-10)):
+        gpg.forward()
+        time.sleep(0.1)
+        gpg.stop()
+
+    print("FUCK YOU")
+    print(int(len(image[0])/2))
+    print(waypoint[0][1] < (int(len(image[0])/2)))
+    print(waypoint[0][1] > (int(len(image[0])/2)))
+    if (waypoint[0][1] < (int(len(image[0])/2))):
+        print("inside if 1")
+        gpg.left()
+        
+        time.sleep(0.05)
+        gpg.stop()
+        print("bottom of if 1")
+    if (waypoint[0][1] > (int(len(image[0])/2))):
+        print("inside if 2")
+        gpg.right()
+       
+        time.sleep(0.05)
+        gpg.stop()
+
+    
+        
 
 def regionOfInterest(image):
     amountTaken = 60
@@ -50,8 +83,9 @@ def regionOfInterest(image):
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
 
-imageHeight = 768
-imageWidth = 1024
+
+imageHeight = 384
+imageWidth = 512
 with picamera.PiCamera() as camera:
     camera.resolution = (imageWidth,imageHeight)
     camera.framerate = 30
@@ -62,10 +96,11 @@ with picamera.PiCamera() as camera:
         canny_image = canny(lane_image)
         croppedImage = regionOfInterest(canny_image)
         waypoints = waypoint_detection(croppedImage)
-        print(waypoints)
-        #lines = cv2.HoughLinesP(croppedImage, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=10)
-        #print(lines)
-        #line_image = display_lines(lane_image, lines)
-        #combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
-        cv2.imshow("asscum",croppedImage)
+        
+        moveGoPiGo(waypoints)
+#        lines = cv2.HoughLinesP(croppedImage, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=10)
+#        #print(lines)
+#        line_image = display_lines(lane_image, lines)
+#        combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+        cv2.imshow("lineVision",canny_image)
         cv2.waitKey(1)
