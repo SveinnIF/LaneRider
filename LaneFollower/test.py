df29d9ce3b7a9b05e7b5a9511d224a09f79e6b3f
SveinnIF <SveinnIF@github.com>
2020-10-13 13:01:38 +0000

    asscum

--- LaneFollower/test.py
+++ LaneFollower/test.py
@@ -7,26 +7,31 @@ from easygopigo3 import EasyGoPiGo3
 # GPG will be the GoPiGo3 object.
 gpg = EasyGoPiGo3()
 
-print("Move the motors forward freely for 1 second.")
 gpg.forward()
-time.sleep(1)
-gpg.stop()
-
-print("Stop the motors for 1 second.")
-time.sleep(1)
-
-print("Drive the motors 50 cm and then stop.")
-gpg.drive_cm(50, True)
-time.sleep(1)
-
-print("Turn right 1 second.")
-gpg.right()
-time.sleep(1)
-
-print("Turn left 1 second.")
 gpg.left()
-time.sleep(1)
-
-print("Stop!")
+time.sleep(5)
 gpg.stop()
-print("Done!")
+
+#print("Move the motors forward freely for 1 second.")
+#gpg.forward()
+#time.sleep(1)
+#gpg.stop()
+#
+#print("Stop the motors for 1 second.")
+#time.sleep(1)
+#
+#print("Drive the motors 50 cm and then stop.")
+#gpg.drive_cm(50, True)
+#time.sleep(1)
+#
+#print("Turn right 1 second.")
+#gpg.right()
+#time.sleep(1)
+#
+#print("Turn left 1 second.")
+#gpg.left()
+#time.sleep(1)
+#
+#print("Stop!")
+#gpg.stop()
+#print("Done!")
