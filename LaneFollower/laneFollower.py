5d53b125e14b79c617b03b3948975e705dee45dc
Robin Kaastrup <32705397+Eltryon@users.noreply.github.com>
2020-11-09 23:20:16 +0100

    Update laneFollower.py

--- LaneFollower/laneFollower.py
+++ LaneFollower/laneFollower.py
@@ -42,7 +42,8 @@ def filterBlackWhite(image):
 
 
 def getCountorPts(image):
-    ret, thresh = cv2.threshold(image, 127, 255, 0)
+    image_grey_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(image_grey_scale, 127, 255, 0)
     im, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     print(contours)
     cnt = contours[0].reshape(-1, 2)
