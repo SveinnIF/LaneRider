370beaa5c8972afb049a322ee375d5f05fe941f5
Robin Kaastrup <32705397+Eltryon@users.noreply.github.com>
2020-11-10 11:25:33 +0100

    Update laneFollower.py

--- LaneFollower/laneFollower.py
+++ LaneFollower/laneFollower.py
@@ -20,14 +20,14 @@ TARGET_W = imageWidth
 
 # Birdseye transform lookup table
 src = np.float32([[0, TARGET_H], [TARGET_W, TARGET_H], [0, 0], [TARGET_W, 0]])
-dst = np.float32([[150, TARGET_H], [490, TARGET_H], [0, 0], [TARGET_W, 0]])
+dst = np.float32([[165, TARGET_H], [475, TARGET_H], [0, 0], [TARGET_W, 0]])
 M = cv2.getPerspectiveTransform(src, dst)
 
 # IMAGE PROCESSING FUNCTIONS
 # Functions that transform the input image
 def birdsEyeTransform(image):
 
-    #img = image[cropTop:imageHeight, 0:imageWidth]
+    img = image[280:(280+TARGET_H), 0:TARGET_W]
     warped_img = cv2.warpPerspective(image, M, (TARGET_W, TARGET_H))
 
     return warped_img
@@ -39,18 +39,18 @@ def birdsEyeTransform(image):
 #     (tresh, image_black_white) = cv2.threshold(image_grey_scale, 127, 255, cv2.THRESH_BINARY)
 #     return image_black_white
 
-# def getCountorPts(image):
-#     image_grey_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     ret, thresh = cv2.threshold(image_grey_scale, 127, 255, 0)
-#     im, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#     print(contours)
-#     cnt = contours[0].reshape(-1, 2)
-#
-#
-#     mask = np.zeros(image.shape, np.uint8)
-#     cv2.drawContours(mask, [cnt], 0, 255, -1)
-#     #pixelpoints = np.transpose(np.nonzero(mask))
-#     #pixelpoints = cv2.findNonZero(mask)
+def getCountorPts(image):
+    image_grey_scale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(image_grey_scale, 127, 255, 0)
+    im, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print(contours)
+    cnt = contours[0].reshape(-1, 2)
+
+
+    mask = np.zeros(image.shape, np.uint8)
+    cv2.drawContours(mask, [cnt], 0, 255, -1)
+    #pixelpoints = np.transpose(np.nonzero(mask))
+    #pixelpoints = cv2.findNonZero(mask)
 
 #def
 
@@ -85,11 +85,12 @@ with picamera.PiCamera() as camera:
         #cropped_image = CropImageFromTop(lane_image, 60)
         #canny_image = canny(lane_image)
         print(lane_image.shape)
-        lane_image = lane_image[280:(280+TARGET_H), 0:TARGET_W]
+
         print(lane_image.shape)
         img_canny = canny(lane_image)
         img_birdseye = birdsEyeTransform(img_canny)
         img_birdseye2 = birdsEyeTransform(lane_image)
+        getCountorPts(img_birdseye)
         #img_blackwhite = filterBlackWhite(img_birdseye)
         #cntPts = getCountorPts(img_canny)
         cv2.imshow("lineVision", img_canny)
