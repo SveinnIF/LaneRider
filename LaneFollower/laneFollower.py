1969ec426871125fcb8af8612baa7a436c566d0a
Robin Kaastrup <32705397+Eltryon@users.noreply.github.com>
2020-11-10 18:45:45 +0100

    Update laneFollower.py

--- LaneFollower/laneFollower.py
+++ LaneFollower/laneFollower.py
@@ -110,8 +110,10 @@ with picamera.PiCamera() as camera:
         img_birdseye = birdsEyeTransform(img_canny)
         img_birdseye2 = birdsEyeTransform(lane_image)
         contours = getCountorPts(img_birdseye)
-        x_arr, y_arr = splitCoordinateArray(contours)
-        print(np.polyfit(x_arr, y_arr, 2))
+        if len(contours) > 0:
+            x_arr, y_arr = splitCoordinateArray(contours)
+            print(np.polyfit(x_arr, y_arr, 2))
+
         #img_convolutional = convolutetheprogram(img_birdseye2)
         # getCountorPts(img_birdseye)
         #img_blackwhite = filterBlackWhite(img_birdseye)
