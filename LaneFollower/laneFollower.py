db274c4b868c33ff84543297eb10bc516eb55399
Robin Kaastrup <32705397+Eltryon@users.noreply.github.com>
2020-11-10 09:33:53 +0100

    Update laneFollower.py

--- LaneFollower/laneFollower.py
+++ LaneFollower/laneFollower.py
@@ -19,7 +19,7 @@ cropBottom = 0
 croppedHeight = imageHeight - cropTop - cropBottom
 
 # Birdseye transform lookup table
-src = np.float32([[cropTop, croppedHeight], [imageWidth, croppedHeight], [cropTop, 0], [imageWidth, 0]])
+src = np.float32([[0, croppedHeight], [imageWidth, croppedHeight], [0, cropTop], [imageWidth, cropTop]])
 dst = np.float32([[imageWidth / 2 - 35, croppedHeight], [imageWidth / 2 + 35, croppedHeight], [0, 0], [imageWidth, 0]])
 M = cv2.getPerspectiveTransform(src, dst)
 
