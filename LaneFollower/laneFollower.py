1652581e76552773d6839656cbd8b2e139b507e0
Robin Kaastrup <32705397+Eltryon@users.noreply.github.com>
2020-11-12 20:09:30 +0100

    Update laneFollower.py

--- LaneFollower/laneFollower.py
+++ LaneFollower/laneFollower.py
@@ -16,7 +16,7 @@ def findContours(image):
     _, newImage = cv2.threshold(image, 100, 255, cv2.COLOR_BGR2GRAY)
     blurredImage = cv2.GaussianBlur(newImage, (5, 5), 0)
     ret, thresh = cv2.threshold(blurredImage, 150,255,cv2.THRESH_BINARY)
-    contours = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours = cv2.findContours(thresh.copy(), cv2.RETR_FLOODFILL, cv2.CHAIN_APPROX_NONE)
     return thresh, contours
 
 def motorControl(image, contours):
