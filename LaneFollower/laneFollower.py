65ce28ff5d9d1473bc8695f21417b8a2d7ea5d01
Robin Kaastrup <32705397+Eltryon@users.noreply.github.com>
2020-11-12 20:51:44 +0100

    Update laneFollower.py

--- LaneFollower/laneFollower.py
+++ LaneFollower/laneFollower.py
@@ -12,12 +12,12 @@ def cropImage(image, top, bottom, left, right):
 
 
 def findContours(image):
-    _, img_gs = cv2.threshold(image, 127, 255, cv2.COLOR_BGR2GRAY)
-    blurredImage = cv2.GaussianBlur(img_gs, (5, 5), 0)
-    #_, img_bw = cv2.threshold(blurredImage, 127, 255, cv2.THRESH_BINARY)
-    #print(img_bw)
-    img_cnt, contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    return img_cnt, contours
+    _, newImage = cv2.threshold(image, 127, 255, cv2.COLOR_BGR2GRAY)
+    blurredImage = cv2.GaussianBlur(newImage, (5, 5), 0)
+    _, img_bw = cv2.threshold(blurredImage, 127, 255, cv2.THRESH_BINARY)
+    print(img_bw)
+    contours, hierarchy = cv2.findContours(img_bw, 1, cv2.CHAIN_APPROX_NONE)
+    return img_bw, contours
 
 def motorControl(image, contours):
     if len(contours) > 0:
@@ -51,10 +51,9 @@ with picamera.PiCamera() as camera:
     image = image.reshape((imageHeight,imageWidth,3))
     for frame in camera.capture_continuous(image, format="bgr", use_video_port=True):
         lane_image = np.copy(image)
-        croppedImage = cropImage(lane_image, 0, 480, 0, 640)
-        cv2.imshow("lineVision", croppedImage)
-        print(croppedImage)
+        croppedImage = cropImage(lane_image, 60, 480, 0, 640)
         thresh, contours = findContours(croppedImage)
-        #motorControl(thresh, contours)
-        #cv2.imshow("lineVision1", thresh)
+        motorControl(thresh, contours)
+        cv2.imshow("lineVision", croppedImage)
+        cv2.imshow("lineVision1", thresh)
         cv2.waitKey(1)
