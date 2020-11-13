8d93a5ddeb568792c13cd7ca0e45105071f082ce
Robin Kaastrup <32705397+Eltryon@users.noreply.github.com>
2020-11-13 17:49:16 +0100

    Update laneFollower.py

--- LaneFollower/laneFollower.py
+++ LaneFollower/laneFollower.py
@@ -14,11 +14,11 @@ def cropImage(image, top, bottom, left, right):
 def findContours(image):
     newImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     blurredImage = cv2.GaussianBlur(newImage, (5, 5), 0)
-    _, img_bw = cv2.threshold(blurredImage, 127, 255, cv2.THRESH_BINARY)
+    _, img_bw = cv2.threshold(blurredImage, 190, 255, cv2.THRESH_BINARY)
     im, contours, hierarchy = cv2.findContours(img_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     return img_bw, contours
 
-def motorControl(image, contours):
+def motorControl(image, imageForDrawing, contours):
     if len(contours) > 0:
         contour_area = max(contours, key=cv2.contourArea)
         moment = cv2.moments(contour_area)
@@ -26,8 +26,8 @@ def motorControl(image, contours):
         cx = int(moment['m10']/moment['m00'])
         cy = int(moment['m01']/moment['m00'])
 
-        cv2.line(image,(cx, 0), (cx,640), (255, 0, 0), 1)
-        cv2.line(image,(0, cy), (280, cy), (255, 0, 0), 1)
+        cv2.line(imageForDrawing,(cx, 0), (cx,640), (255, 0, 0), 1)
+        cv2.line(imageForDrawing,(0, cy), (280, cy), (255, 0, 0), 1)
 
         cv2.drawContours(image, contours, -1, (0, 255, 0), 1)
 
@@ -52,7 +52,7 @@ with picamera.PiCamera() as camera:
     image = image.reshape((imageHeight,imageWidth,3))
     for frame in camera.capture_continuous(image, format="bgr", use_video_port=True):
         lane_image = np.copy(image)
-        croppedImage = cropImage(lane_image, 200, 480, 0, 640)
+        croppedImage = cropImage(lane_image, 200, 480, 40, 600)
         thresh, contours = findContours(croppedImage)
         motorControl(thresh, contours)
         cv2.imshow("lineVision", croppedImage)
